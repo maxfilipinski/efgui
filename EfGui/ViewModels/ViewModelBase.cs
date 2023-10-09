e6d4048a36efe67264ec34f709db681b71d5d112
mfilipinski <filipinskim@gmail.com>
2023-10-09 22:38:14 +0200

    Dodaj pliki projektów.

---
+++ EfGui/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace EfGui.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ EfGui/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace EfGui.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
